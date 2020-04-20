1020eca31a46a2a2faabdef8b704fb52893c75e3
yujihua <yujh85@chinaunicom.cn>
2020-04-20 19:29:03 +0800

    first commit

---
+++ pages/demo/demo.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<view class="icon iconfont icon-manyidu animated fadeInDownBig"></view>
+		<view class="icon iconfont icon-fenlei animated fadeIn"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<button type="default" @click="toDetail">点击按钮跳转</button>
+		<navigator url="../demo/demo">
+			<button type="primary">跳转到新页面</button>
+		</navigator>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			toDetail(){
+				uni.navigateTo({
+					url:'../demo/demo'
+				})
+			}
+		}
+		
+	}
+</script>
+
+<style>
+</style>
